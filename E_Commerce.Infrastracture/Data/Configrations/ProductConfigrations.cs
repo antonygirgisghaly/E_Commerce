f5232474dcb0bc6f9d3d4efca8e4f62647457092
Antony Girgis <antony26429122@gmail.com>
2026-06-27 20:50:25 +0300

    feat: configure EF Core DbContext and ProductConfigration

---
+++ E_Commerce.Infrastracture/Data/Configrations/ProductConfigrations.cs
@@ -0,0 +1,35 @@
+﻿using E_Commerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastracture.Data.Configrations
+{
+    internal class ProductConfigrations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.ProductBrand)
+                   .WithMany()
+                   .HasForeignKey(x => x.BrandId);
+            builder.HasOne(p => p.ProductType)
+                   .WithMany()
+                   .HasForeignKey(p => p.TypeId);
+
+            builder.Property(p => p.Name)
+                   .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                   .HasMaxLength(500);
+
+            builder.Property(p => p.PictureUrl)
+                   .HasMaxLength(200);
+            builder.Property(p => p.Price)
+                   .HasPrecision(18, 2);
+        }
+    }
+}
---
+++ E_Commerce.Infrastracture/InfrastractureServicesRegisteration.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce.Infrastracture.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastracture
+{
+    public static class InfrastractureServicesRegisteration
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services,IConfiguration configration)
+        {
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(configration.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
