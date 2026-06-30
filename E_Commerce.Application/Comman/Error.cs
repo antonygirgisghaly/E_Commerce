2d9aebae5737ec676ccb81d6856fd02e8a0cff99
Antony Girgis <antony26429122@gmail.com>
2026-06-30 11:18:23 +0300

    feat : Implment Result desgin pattern

---
+++ E_Commerce.Application/Comman/Error.cs
@@ -0,0 +1,31 @@
+﻿namespace E_Commerce.Application.Comman
+{
+    public sealed record Error(string code, string description, ErrorType errorType = ErrorType.Failure)
+    {
+        public static Error Failure(string code = "General.Failure",string description = "General Failure Error Has Occured")
+            => new(code, description);
+        public static Error Validation(string code = "General.Validation", string description = "General Validation Error Has Occured")
+            => new(code, description,ErrorType.Validation);
+        public static Error NotFound(string code = "General.NotFound", string description = "Resorce Has Occured")
+          => new(code, description, ErrorType.NotFound);
+        public static Error Conflict(string code = "General.Conflict", string description = "General Conflict Error Has Occured")
+          => new(code, description, ErrorType.Conflict);
+        public static Error Unothorized(string code = "General.Unothorized", string description = "Access Is Denied")
+          => new(code, description, ErrorType.Unauthorized);
+        public static Error Forbidden(string code = "General.Forbidden", string description = "This O")
+          => new(code, description, ErrorType.Forbidden);
+        public static Error InvalidCredentials(string code = "General.InvalidCredentials", string description = "Provided Credentials Invalid")
+          => new(code, description, ErrorType.InvalidCredentials);
+    }
+
+    public enum ErrorType
+    {
+        Failure = 0,
+        Validation = 1,
+        NotFound = 2,
+        Conflict = 3,
+        Unauthorized = 4,
+        Forbidden = 5,
+        InvalidCredentials = 6
+    }
+}
---
+++ E_Commerce.Application/Comman/Result.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Application.Comman
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public IReadOnlyList<Error> Errors { get; } 
+
+        protected Result(bool isSuccess,IReadOnlyList<Error> errors) 
+        {
+            IsSuccess = isSuccess;
+            Errors = errors;
+        }
+        public static Result Ok() => new(true, Array.Empty<Error>());
+        public static Result Fail(Error errors) => new(false, new[] {errors});
+        public static Result Fail(IReadOnlyList<Error> errors) => new(false, errors);
+    }
+    public class Result<TValue> : Result
+    {
+        private readonly TValue _value;
+
+        public TValue Data => IsSuccess ? _value : throw new InvalidOperationException("Can not access value of failed result");
+        private Result(TValue value) : base(true,Array.Empty<Error>())
+        {
+            _value = value;
+        }
+        private Result(Error error) : base(true, new[] {error })
+        {
+            _value = default!;
+        }
+        private Result(IReadOnlyList<Error> error) : base(true, error)
+        {
+            _value = default!;
+        }
+        public static Result<TValue> Ok(TValue value) => new Result<TValue>(value);
+        public static Result<TValue> Fail(Error errors) => new Result<TValue>(errors);
+        public static Result<TValue> Fail(IReadOnlyList<Error> errors) => new Result<TValue>(errors);
+
+        public static implicit operator Result<TValue>(TValue value) => Ok(value);
+        public static implicit operator Result<TValue>(Error error) => Fail(error);
+
+    }
+}
