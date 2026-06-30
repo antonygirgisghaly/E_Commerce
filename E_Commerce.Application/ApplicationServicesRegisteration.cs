c77ce271d1ffe6fe30459e18e17982dc592ffc9e
Antony Girgis <antony26429122@gmail.com>
2026-06-30 11:18:54 +0300

    feat : add ProductServices

---
+++ E_Commerce.Application/ApplicationServicesRegisteration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.Application.Contracts;
+using E_Commerce.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+namespace E_Commerce.Application
+{
+    public static class ApplicationServicesRegisteration
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection service)
+        {
+            service.AddAutoMapper(c => { },typeof(ApplicationServicesRegisteration).Assembly);
+            service.AddScoped<IProductService, ProductService>();
+            return service;
+        }
+    }
+}
---
+++ E_Commerce.Application/Services/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using E_Commerce.Application.Comman;
+using E_Commerce.Application.Contracts;
+using E_Commerce.Application.DTOs.Products;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Application.Services
+{
+    internal class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductService(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<Result<IReadOnlyList<BrandDto>>> GetAllBrandsAsync(CancellationToken ct = default)
+        {
+            var brands = await _unitOfWork.GetGenericRepository<ProductBrand,int>().GetAllAsync(ct);
+            var data = _mapper.Map<IReadOnlyList<BrandDto>>(brands);
+            return Result<IReadOnlyList<BrandDto>>.Ok(data);
+        }
+
+        public async Task<Result<IReadOnlyList<ProductDto>>> GetAllProductsAsync(CancellationToken ct = default)
+        {
+            var brands = await _unitOfWork.GetGenericRepository<Product,int>().GetAllAsync(ct);
+            var data = _mapper.Map<IReadOnlyList<ProductDto>>(brands);
+            return Result<IReadOnlyList<ProductDto>>.Ok(data);
+        }
+
+        public async Task<Result<IReadOnlyList<TypeDto>>> GetAllTypesAsync(CancellationToken ct = default)
+        {
+            var types = await _unitOfWork.GetGenericRepository<ProductType,int>().GetAllAsync(ct);
+            var data = _mapper.Map<IReadOnlyList<TypeDto>>(types);
+            return Result<IReadOnlyList<TypeDto>>.Ok(data);
+        }
+
+        public async Task<Result<ProductDto>> GetProductByIdAsync(int id, CancellationToken ct = default)
+        {
+            var product = await _unitOfWork.GetGenericRepository<Product,int>().GetByIdAsync(id,ct);
+            if(product == null)
+            {
+                return Error.NotFound("Product.NotFound",$"Product with{id} Not Found");
+            }
+            return _mapper.Map<ProductDto>(product);
+        }
+    }
+}
