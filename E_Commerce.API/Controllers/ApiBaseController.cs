ea69f296d94f362e7057d8400a5d91fa2c22fa75
Antony Girgis <antony26429122@gmail.com>
2026-07-01 10:27:19 +0300

    feat : Add ApiBaseController

---
+++ E_Commerce.API/Controllers/ApiBaseController.cs
@@ -0,0 +1,48 @@
+﻿using E_Commerce.Application.Comman;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+        public static ActionResult ToActionResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+                return new OkObjectResult(result);
+            else
+                return ToProblem(result.Errors);
+        }
+        public static ActionResult ToActionResult(Result result)
+        {
+            if (result.IsSuccess)
+                return new OkResult();
+            else
+                return ToProblem(result.Errors);
+        }
+        protected static ObjectResult ToProblem(IReadOnlyList<Error> errors)
+        {
+            var firstError = errors[0];
+
+            var statuscode = firstError.errorType switch
+            {
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+                ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+                _=> StatusCodes.Status500InternalServerError
+            };
+            var problems = new ProblemDetails
+            {
+                Status = statuscode,
+                Type = firstError.code,
+                Detail = firstError.description,
+                Extensions = { ["Errors"] = errors}
+            };
+            return new ObjectResult(problems) { StatusCode = statuscode};
+        }
+    }
+}
